116da6db66250625d2d83d59d2c6f93d91eb25f7
Florian Blaschke <florianblaschke@web.de>
2023-06-20 12:21:02 +0200

    finished

--- components/CarRace/index.js
+++ components/CarRace/index.js
@@ -8,16 +8,35 @@ import {
   Track,
 } from "./CarRace.styled";
 import { initialCars, getRandomDistance } from "../../utils/utils";
+import { useImmer } from "use-immer";
 
 const finishLine = 200;
 
 export default function CarRace() {
-  const [cars, setCars] = useState(initialCars);
+  const [cars, setCars] = useImmer(initialCars);
 
   function moveCar(clickedCar) {
     const coveredDistance = getRandomDistance();
     console.log("clickedCar", clickedCar);
     console.log("coveredDistance", coveredDistance);
+    // setCars(
+    //   cars.map((car) =>
+    //     car.emoji === clickedCar.emoji
+    //       ? {
+    //           ...car,
+    //           position: {
+    //             lastDistance: coveredDistance,
+    //             x: car.position.x + car.position.lastDistance,
+    //           },
+    //         }
+    //       : car
+    //   )
+    // );
+    setCars((draft) => {
+      const selectedCar = draft.find(({ emoji }) => emoji === clickedCar.emoji);
+      selectedCar.position.lastDistance = coveredDistance;
+      selectedCar.position.x += selectedCar.position.lastDistance;
+    });
   }
 
   const winner = cars.find((car) => car.position.x >= finishLine);
